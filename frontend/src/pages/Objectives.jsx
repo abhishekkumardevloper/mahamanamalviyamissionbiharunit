4a605ea9fc5f041b34c9df1794f5c7397d994257
abhishekkumardevloper <abhishekkumar.devloper@gmail.com>
2026-03-05 17:29:43 +0530

    Update Objectives.jsx

--- frontend/src/pages/Objectives.jsx
+++ frontend/src/pages/Objectives.jsx
@@ -12,7 +12,7 @@ const Team = () => {
         { id: 25, image: "/images/team25.jpg", designation: "Patrons, Bihar", name: "Shri O. P. Srivastava" },
         { id: 26, image: "/images/team26.jpg", designation: "Patrons", name: "Shri Omprakash Yadav" },
         { id: 27, image: "/images/team27.jpg", designation: "Patrons", name: "Shri Uday Singh" },
-        { id: 28, image: "/images/team28.jpg", designation: "Patrons", name: "Prof. T. N. Singh" }
+        { id: 28, image: "/tnsingh.jpeg", designation: "Patrons", name: "Prof. T. N. Singh" }
       ]
     },
     {
@@ -36,7 +36,7 @@ const Team = () => {
         { id: 15, image: "/images/team15.jpg", designation: "Secretary", name: "Rajneesh Upadhyay" },
         { id: 16, image: "/images/team16.jpg", designation: "Secretary", name: "Amarendra Pandey" },
         { id: 17, image: "/ravi.jpeg", designation: "Secretary", name: "Rajeev Kumar" },
-        { id: 18, image: "/images/team18.jpg", designation: "Secretary", name: "Shailendra Tomar" },
+        { id: 18, image: "/tomar.jpeg", designation: "Secretary", name: "Shailendra Tomar" },
         { id: 19, image: "/pankhuri.jpeg", designation: "Secretary", name: "Dr. Pankhuri Mishra" },
         { id: 20, image: "/sumit.jpeg", designation: "Secretary", name: "Sumit Singh" },
         { id: 21, image: "/images/team21.jpg", designation: "Secretary", name: "Ankit Kumar" },
