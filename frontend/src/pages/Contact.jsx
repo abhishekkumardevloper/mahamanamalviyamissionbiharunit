63bda188feab0ae06c452d18e4ade8c881640442
abhishekkumardevloper <abhishekkumar.devloper@gmail.com>
2026-04-06 15:05:17 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -84,7 +84,7 @@ const Contact = () => {
             {/* Map */}
             <div className="bg-white border-2 border-[#F4C430] rounded-lg overflow-hidden shadow-lg">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28792.528868621797!2d85.05055232397446!3d25.569467271072735!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ed57d56ee2ec61%3A0xaa59a2e2ac9b8d22!2sPhulwari%20Sharif%2C%20Patna%2C%20Bihar!5e0!3m2!1sen!2sin!4v1775467129170!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d472.56646845799855!2d85.03327304569397!3d25.561545388451034!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f2a9edfd9c7c1d%3A0x2e74b8c58b242e2c!2sHari%20Nagar%20township%2C%20Phuliya%20tola%20road%2C%20near%20AIIMS%20Road%2C%20Patna%2C%20Bihar%20801505!5e0!3m2!1sen!2sin!4v1775468014772!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
                 width="100%"
                 height="100%"
                 style={{ border: 0, minHeight: '500px' }}
