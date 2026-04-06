e02945c37310522284e82ddbeffeafd2bc19084b
abhishekkumardevloper <abhishekkumar.devloper@gmail.com>
2026-04-06 17:40:27 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -298,6 +298,9 @@ const Home = () => {
                 <h3 className="text-4xl md:text-5xl font-extrabold text-[#111111] mb-8" style={getFontStyle()}>
                   {t('leadership.leaderName')}
                 </h3>
+                <h5 className="text-4xl md:text-5xl font-extrabold text-[#111111] mb-8" style={getFontStyle()}>
+                  {t('leadership.post')}
+                </h5>
                 <blockquote className="text-xl md:text-2xl text-gray-700 italic mb-10 leading-relaxed border-l-4 border-[#F4C430] pl-6 md:pl-8 py-2 relative" style={getFontStyle()}>
                   <span className="absolute -top-4 -left-2 text-6xl text-[#F4C430]/20 font-serif">"</span>
                   {t('leadership.philosophy')}
